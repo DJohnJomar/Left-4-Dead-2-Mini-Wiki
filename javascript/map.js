7bc1909c59f249ed7e42376f029af879bfc14e65
John Jomar Dimaunahan <johndjomar@gmail.com>
2024-02-15 02:57:05 +0800

    Added toggle functionality

--- javascript/map.js
+++ javascript/map.js
@@ -1,16 +1,21 @@
 let isShown = false;
-const button = document.querySelector(".btnShowHide");
-const tdImage = document.querySelector(".tdImage");
-button.onclick = showHide;
+document.querySelectorAll(".btnShowHide").forEach(button => {
+    button.addEventListener('click', toggleVisibility);
+});
 
-function showHide(){
-    if(isShown == false){
-        button.innerText = "[Hide]";
-        tdImage.style.display = "block";
+function toggleVisibility(event) {
+    const button = event.currentTarget;
+    const targetId = button.getAttribute('data-target');
+    const targetImage = document.getElementById(targetId);
+
+    if (targetImage) {
+        if (isShown) {
+            button.innerText = "[Show]";
+            targetImage.style.display = "none";
+        } else {
+            button.innerText = "[Hide]";
+            targetImage.style.display = "block";
+        }
+        isShown = !isShown;
     }
-    if(isShown == true){
-        button.innerText = "[Show]";
-        tdImage.style.display = "none";
-    }
-    isShown = !isShown;
 }
